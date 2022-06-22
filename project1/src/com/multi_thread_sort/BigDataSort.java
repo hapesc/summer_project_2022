8cade97a2d981f14526a02590cfa85993be1efeb
hapesc <yufan_liang@icloud.com>
2022-06-22 16:22:20 +0800

    实现了单次的多线程归并排序（size=10w） 下一步： 1.将临时文件归并成总文件 2。将2路归并优化为k路归并

---
+++ project1/src/com/multi_thread_sort/BigDataSort.java
@@ -0,0 +1,75 @@
+package com.multi_thread_sort;
+
+import javax.swing.*;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.ForkJoinPool;
+
+public class BigDataSort {
+    public static void main(String[] args) throws FileNotFoundException {
+        String filePath="/Users/michael-liang/Desktop/IO_Test/TestSet_A.txt";
+        String tempPath="/Users/michael-liang/Desktop/IO_Test/TestSet_A_temp/temp";
+        String[] FilePaths=new String[26];
+        sortSingleFile(filePath,tempPath);
+    }
+
+    public static void sortSingleFile(String filePath,String tempPath) throws FileNotFoundException {
+//
+        int batchSize=500000;
+        int eof = 0;
+        long start = System.currentTimeMillis();
+        int cnt=0,PathCnt=0;
+        String[] buffer = new String[batchSize];
+        try (Reader data = new InputStreamReader(Files.newInputStream(Paths.get(filePath)));
+
+        ) {
+
+            while(eof!=-1) {
+                Writer output = new FileWriter(tempPath+ PathCnt +".txt");
+//      单次读取100000行
+                if(cnt>=batchSize) {
+                    buffer = new String[batchSize];
+                    PathCnt++;
+                }
+
+                char[] temp = new char[16];
+                for ( cnt = 0; cnt < batchSize; cnt++) {
+                    eof = data.read(temp, 0, 16);
+                    buffer[cnt] = String.valueOf(temp);
+                }
+
+//      多线程归并排序
+                ForkJoinPool pool = new ForkJoinPool(); // 分配一个并行线程池
+                SortTask sortTask = new SortTask(buffer, new StringSort());
+                pool.execute(sortTask);
+                String[] result = sortTask.join();
+
+                for (String a : result)
+                    output.write(a);
+                System.out.println("success"+PathCnt);
+                output.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("error");
+        } finally {
+            System.out.println(System.currentTimeMillis() - start);
+        }
+//        mergeTempFile(PathCnt,tempPath);
+
+    }
+
+    private static void mergeTempFile(int num,String dir) throws FileNotFoundException {
+        String[] merged=new String[num];
+        Reader[] tempFile=new Reader[num];
+        int eof=0;
+        for(int i=0;i<num;i++){
+            tempFile[i]=new FileReader(dir+i+".txt");
+        }
+
+
+    }
+
+}
